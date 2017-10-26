39e285a9d2ccd8759ff6e017d3e9895fa96ff70b
KadoTomohiro <corner0427@gmail.com>
2017-10-27 00:05:37 +0900

    add issue model

---
+++ Issues/Model/Issue.cs
@@ -0,0 +1,9 @@
+﻿namespace Issues.Model
+{
+    public sealed class Issue
+    {
+        public string Id { get; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
